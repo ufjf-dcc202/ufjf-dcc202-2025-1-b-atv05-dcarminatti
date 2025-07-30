85737f2c33c65be4873809eb0f9390faaf2528c1
dcarminatti <dctti.amarante@gmail.com>
2025-07-30 16:48:07 -0300

    adiciona cria discos

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ eH1.textContent = "Olá, DCC202!";
 const eTabuleiro = criaTabuleiro();
 document.body.appendChild(eTabuleiro);
 
+for (let i = 0; i < 7; i++) {
+  const eDisco = criaDiscoTabuleiro();
+  eTabuleiro.appendChild(eDisco);
+}
+
 function criaTabuleiro() {
   const novoTabuleiro = document.createElement("div");
   novoTabuleiro.classList.add("tabuleiro");
   return novoTabuleiro;
+}
+
+function criaDiscoTabuleiro() {
+  const novoDisco = document.createElement("div");
+  novoDisco.classList.add("discoTabuleiro");
+  return novoDisco;
 }
