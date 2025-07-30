54bad49b5c1752a7858423a7db42953d1cdd51b7
dcarminatti <dctti.amarante@gmail.com>
2025-07-30 16:38:39 -0300

    criado estilos via js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const eH1 = document.querySelector("h1");
 eH1.textContent = "Olá, DCC202!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.appendChild(eTabuleiro);
+
+function criaTabuleiro() {
+  const novoTabuleiro = document.createElement("div");
+  novoTabuleiro.classList.add("tabuleiro");
+  novoTabuleiro.style.backgroundColor = "lightgreen";
+  novoTabuleiro.style.width = "175px";
+  novoTabuleiro.style.minHeight = "25px";
+  return novoTabuleiro;
+}
